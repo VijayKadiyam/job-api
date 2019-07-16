09d75d2ca82dc56ac1ab07876a14b890ae3b7214
VijayKadiyam <freelancervijayk@gmail.com>
2019-07-17 01:07:50 +0530

    Register return changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,8 @@ class RegisterController extends Controller
       'name'                    => $data['name'],
       'email'                   => $data['email'],
       'password'                => Hash::make($data['password']),
-      'phone'                   =>  $data['phone']
+      'phone'                   =>  $data['phone'],
+      'active'                  =>  1
     ]);
   }
 
