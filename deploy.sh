d334c6af04cc9780217809543cedf6587763946a
VijayKadiyam <freelancervijayk@gmail.com>
2020-01-14 11:38:53 +0530

    Latest commit

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cd ~/public_html/digiloop-api/
+cd ~/public_html/rms-api/
 
-#chmod -R 777 storage public vendor bootstrap database .git
+chmod -R 777 storage public vendor bootstrap database .git
+
+cd ~/public_html/
+
+chmod -R 755 rms-api
